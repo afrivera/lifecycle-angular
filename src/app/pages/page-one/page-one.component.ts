f55a2c738376bc75ba67abdaf2348a06f4287fea
afrivera <andresrfelip@hotmail.com>
2022-06-14 16:21:18 -0500

    end session 20 angular udemy course

---
+++ src/app/pages/page-one/page-one.component.ts
@@ -0,0 +1,51 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-page-one',
+  templateUrl: './page-one.component.html',
+  styles: [
+  ]
+})
+export class PageOneComponent implements OnInit, OnChanges, DoCheck,
+ AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  name: string = 'Andres';
+  seconds : number = 0;
+  timerSubscription! : Subscription;
+
+  constructor() {
+    console.log('constructor')
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+    this.timerSubscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.timerSubscription = interval( 1000 ).subscribe( i => this.seconds = i)
+  }
+
+  save(){
+
+  }
+}
